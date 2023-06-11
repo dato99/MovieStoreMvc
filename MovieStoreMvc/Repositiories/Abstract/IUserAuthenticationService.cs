71ab74273cdbf8434aaadcb5b5c68c945c85a643
Dato Shengelia <datoshengelia999@gmail.com>
2023-06-11 20:30:35 +0400

    Add project files.

---
+++ MovieStoreMvc/Repositiories/Abstract/IUserAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using MovieStoreMvc.Models.DTO;
+
+namespace MovieStoreMvc.Models.Repositories.Abstract
+{
+    public interface IUserAuthenticationService
+    {
+        Task<Status> LoginAsync(LoginModel model);
+        Task logoutAsync();
+        Task LogoutAsync();
+        Task<Status> RegisterAsync();
+        Task<Status> RegisterAsync(RegistrationModel model);
+        //Task<Status> ChangePasswordAsync(ChangePasswordModel model, string username);
+    }
+}
